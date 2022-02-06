ef1d07279d6a168b6005a6fecae5d2c74575c05e
F1restar4 <19477903+F1restar4@users.noreply.github.com>
2022-02-05 21:26:04 -0600

    Initial commit

---
+++ Entities.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Firestar4.OpenTDBWrapper.Entities
+{
+	public class TriviaQuestion
+	{
+		[JsonProperty]
+		public string Category { get; internal set; }
+
+		[JsonProperty]
+		public string Type { get; internal set; }
+
+		[JsonProperty]
+		public string Difficulty { get; internal set; }
+
+		[JsonProperty]
+		public string Question { get; internal set; }
+
+		[JsonProperty("correct_answer")]
+		public string CorrectAnswer { get; internal set; }
+
+		[JsonProperty("incorrect_answers")]
+		public List<string> IncorrectAnswers { get; internal set; }
+
+		[JsonConstructor]
+		TriviaQuestion(string category, string type, string difficulty, string question, string correctAnswer, List<string> incorrectAnswers)
+		{
+			this.Category = category;
+			this.Type = type;
+			this.Difficulty = difficulty;
+			this.Question = question;
+			this.CorrectAnswer = correctAnswer;
+			this.IncorrectAnswers = incorrectAnswers;
+		}
+	}
+
+	internal class TriviaQuestionResponse
+	{
+		[JsonProperty]
+		public ResponseCode response_code { get; internal set; }
+
+		[JsonProperty]
+		public List<TriviaQuestion> results { get; internal set; }
+
+		[JsonConstructor]
+		internal TriviaQuestionResponse(int response_code, List<TriviaQuestion> results)
+		{
+			this.response_code = (ResponseCode)response_code;
+			this.results = results;
+		}
+
+	}
+
+	enum ResponseCode
+	{
+		Success = 0,
+		NoResults = 1,
+		InvalidParameter = 2,
+		TokenNotFound = 3,
+		TokenEmpty = 4
+	}
+}
---
+++ Rest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Linq;
+using Firestar4.OpenTDBWrapper.Entities;
+namespace Firestar4.OpenTDBWrapper
+{
+	public static class OpenTDBWrapper
+	{
+		public static async Task<TriviaQuestion> GetQuestionAsync()
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://opentdb.com/api.php?amount=1&type=multiple");
+			request.ContentType = "application/json";
+			string data;
+			WebResponse response = request.GetResponse();
+			using (Stream responseStream = response.GetResponseStream() ?? Stream.Null)
+			using (StreamReader responseReader = new StreamReader(responseStream))
+			{
+				data = await responseReader.ReadToEndAsync();
+			}
+			var outResponse = JsonConvert.DeserializeObject<TriviaQuestionResponse>(data);
+			if (outResponse.response_code == ResponseCode.Success)
+			{
+				var first = outResponse.results.First();
+				first.Question = HttpUtility.HtmlDecode(first.Question);
+				first.CorrectAnswer = HttpUtility.HtmlDecode(first.CorrectAnswer);
+				first.IncorrectAnswers = first.IncorrectAnswers.Select(xr => HttpUtility.HtmlDecode(xr)).ToList();
+				first.Difficulty = first.Difficulty[0].ToString().ToUpper() + first.Difficulty.Substring(1);
+				return first;
+			}
+
+			throw new Exception($"Error code: {outResponse.response_code} from the server");
+			
+		}
+	}
+}
